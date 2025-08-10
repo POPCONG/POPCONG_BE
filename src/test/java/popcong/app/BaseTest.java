fa09bf4902552ba6831b4ee2ea998d71fd9036b9
seoiiwon <wldnjstj99@gmail.com>
2025-08-10 17:11:36 +0900

    ✅ Test: "Database 연결 테스트 코드 작성"
    BaseTest로 local에서 공통적으로 활용될 어노테이션 세팅
    DatabaseConnectionTest로 DB 연결, 정보 확인 테스트
    
    related to: #1

---
+++ src/test/java/popcong/app/BaseTest.java
@@ -0,0 +1,11 @@
+package popcong.app;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles({"local", "secret"})
+public abstract class BaseTest {
+}
