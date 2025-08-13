4261b7d15288e830732cae09a3d2b8962bb9632c
seoiiwon <wldnjstj99@gmail.com>
2025-08-13 12:14:30 +0900

    ✨ feature: "entity, model 작성"
    User 모델,
    Provider, Role, UserRole 작성,
    User JPA entity 작성
    
    related to: #5

---
+++ src/main/java/popcong/app/domain/user/model/Role.java
@@ -0,0 +1,6 @@
+package popcong.app.domain.user.model;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/popcong/app/domain/user/model/UserRole.java
@@ -0,0 +1,7 @@
+package popcong.app.domain.user.model;
+
+public enum UserRole {
+    GUEST, // 일반 사용자 (임차인)
+    PENDING, // 호스트 신청 서류 제출자
+    HOST // 호스트 (임대인)
+}
