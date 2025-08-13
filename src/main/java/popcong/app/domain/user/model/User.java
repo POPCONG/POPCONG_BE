8a3daebc34afed23010e24fe7017eed41d8b7484
seoiiwon <wldnjstj99@gmail.com>
2025-08-13 12:22:08 +0900

    🔨 fix: "model class -> record 수정"
    기존에 class와 Getter, Builder로 작성된 model을 record로 수정함
    
    related to: #5

--- src/main/java/popcong/app/domain/user/model/User.java
+++ src/main/java/popcong/app/domain/user/model/User.java
@@ -1,22 +1,17 @@
 package popcong.app.domain.user.model;
 
-import lombok.Builder;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
-@Getter
-@Builder
-public class User {
-    private final Long id;
-    private final Provider provider; // 소셜로그인 제공자
-    private final String providerId; // 소셜로그인에서 제공받은 ID
-    private final String email; // 소셜로그인 email
-    private final String name;
-    private final String profileImageUrl;
-    private final String introduction;
-    private final Role role; // user-admin
-    private final UserRole userRole; // guest-host
-    private final LocalDateTime createdAt;
-    private final LocalDateTime deletedAt;
-}
+public record User (
+    Long id,
+    Provider provider, // 소셜로그인 제공자
+    String providerId, // 소셜로그인에서 제공받은 ID
+    String email, // 소셜로그인 email
+    String name,
+    String profileImageUrl,
+    String introduction,
+    Role role, // user-admin
+    UserRole userRole, // guest-host
+    LocalDateTime createdAt,
+    LocalDateTime deletedAt
+) {}
