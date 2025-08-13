4e62b041a227a572d0a0d10812a7102331edbedf
seoiiwon <wldnjstj99@gmail.com>
2025-08-13 15:17:41 +0900

    ✨ feature: "JWT 유틸리티 작성"
    JWT 유틸리티 작성
    토큰 생성, 유효성 검증, Claims 추출, 인증 정보 추출
    
    related to: #5

---
+++ src/main/java/popcong/app/adapter/out/jwt/JwtUtils.java
@@ -0,0 +1,97 @@
+package popcong.app.adapter.out.jwt;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import popcong.app.domain.auth.model.AuthInfo;
+import popcong.app.domain.auth.port.in.TokenGenerateUseCase;
+
+import javax.crypto.SecretKey;
+import java.util.Collections;
+
+@Slf4j
+@Component
+public class JwtUtils {
+
+    private final SecretKey secretKey;
+    public final TokenGenerateUseCase accessTokenGenerateService;
+    public final TokenGenerateUseCase refreshTokenGenerateService;
+
+    public JwtUtils(
+            SecretKey secretKey,
+            @Qualifier("accessTokenGenerateService") TokenGenerateUseCase accessTokenGenerateService,
+            @Qualifier("refreshTokenGenerateService")  TokenGenerateUseCase refreshTokenGenerateService
+    ) {
+        this.secretKey = secretKey;
+        this.accessTokenGenerateService = accessTokenGenerateService;
+        this.refreshTokenGenerateService = refreshTokenGenerateService;
+    }
+
+    // Access Tokne 생성
+    public String generateAccessToken(AuthInfo authInfo) {
+        return accessTokenGenerateService.generateToken(authInfo);
+    }
+
+    // Refresh Token 생성
+    public String generateRefreshToken(AuthInfo authInfo) {
+        return refreshTokenGenerateService.generateToken(authInfo);
+    }
+
+    // Token 유효성 검증
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(token);
+
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 서명입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+
+        return false;
+    }
+
+    // JWT Claims 추출
+    private Claims getClaimsFromToken(String accessToken) {
+        try {
+            return Jwts.parserBuilder()
+                    .setSigningKey(secretKey)
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+    // Access Token에서 인증 정보 추출
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = getClaimsFromToken(accessToken);
+        String role = claims.get("role", String.class);
+
+        User principal = new User(
+                claims.getSubject(),
+                "",
+                Collections.singleton(new SimpleGrantedAuthority(role))
+        );
+
+        return new UsernamePasswordAuthenticationToken(
+                principal,
+                accessToken,
+                principal.getAuthorities()
+        );
+    }
+}
