6d52612a3f6058ee7d91569276807225dce07d62
seoiiwon <wldnjstj99@gmail.com>
2025-08-18 19:20:18 +0900

    ✨ Feature: "컨트롤러 작성"
    인기 매물 리스트, 현 위치 기반 근처 팝업 정보 리스트 반환 API 컨트롤러 작성
    
    related to: #14

--- src/main/java/popcong/app/adapter/in/user/HomeController.java
+++ src/main/java/popcong/app/adapter/in/user/HomeController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import popcong.app.application.space.dto.response.HomeSpaceListResponseDto;
+import popcong.app.application.space.port.in.SpaceQueryUseCase;
 import popcong.app.application.user.dto.response.HomeUserResponseDto;
 import popcong.app.application.user.port.in.GetHomeInfoUseCase;
 import popcong.app.domain.user.model.User;
@@ -22,6 +24,7 @@ import popcong.app.global.exception.error.AuthErrorCode;
 public class HomeController {
 
     private final GetHomeInfoUseCase getHomeInfoUseCase;
+    private final SpaceQueryUseCase spaceQueryUseCase;
 
     @GetMapping("/me")
     public ResponseDto<HomeUserResponseDto> getHomeInfo(
@@ -39,6 +42,30 @@ public class HomeController {
         return new ResponseDto<>(
                 HttpStatus.OK.value(),
                 "홈 정보 조회 성공",
+                result
+        );
+    }
+
+    // 홈 매물 정보 리스트
+    @GetMapping("/location")
+    public ResponseDto<HomeSpaceListResponseDto> getSpaceLocation(
+            @AuthenticationPrincipal User user,
+            @RequestParam(name = "latitude") Double latitude,
+            @RequestParam(name = "longitude") Double longitude
+    ) {
+        if (user == null) {
+            throw new BusinessException(AuthErrorCode.UNAUTHORIZED);
+        }
+
+        Long currentUserId = user.userId();
+
+        HomeSpaceListResponseDto result = spaceQueryUseCase.loadHomeSpaceList(
+                currentUserId, latitude, longitude
+        );
+
+        return new ResponseDto<>(
+                HttpStatus.OK.value(),
+                "지정 위치 기반 매물, 팝업 정보",
                 result
         );
     }
