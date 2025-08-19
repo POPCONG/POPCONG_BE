948da8e95a4ab20faf42b9384f36a689e5c3e497
seoiiwon <wldnjstj99@gmail.com>
2025-08-20 01:07:10 +0900

    ✨ Feature: "채팅 구현"
    Websocket으로 채팅 구현 완료
    
    related to: #43

--- src/main/java/popcong/app/infra/config/security/SecurityConfig.java
+++ src/main/java/popcong/app/infra/config/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package popcong.app.infra.config.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -39,7 +40,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 // 세션 상태 필요 (oauth2)
