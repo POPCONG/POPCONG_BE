fc1ecd77f77c34468df42250ef51fa8cb784a7b2
chanition <chanyeong1206@gmail.com>
2025-08-20 12:07:40 +0900

    🔨 Fix : "매물 상세 정보 조회"
    매물 상세 정보 조회 수정
    
    related to: #53

--- src/main/java/popcong/app/adapter/in/space/SpaceDetailController.java
+++ src/main/java/popcong/app/adapter/in/space/SpaceDetailController.java
@@ -1,15 +1,16 @@
 package popcong.app.adapter.in.space;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import popcong.app.application.space.dto.response.SpaceDetailResponseDto;
 import popcong.app.application.space.service.GetSpaceDetailService;
 import popcong.app.domain.user.model.User;
+import popcong.app.global.dto.ResponseDto;
 import popcong.app.global.exception.custom.BusinessException;
 import popcong.app.global.exception.error.AuthErrorCode;
 
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,19 +20,18 @@ public class SpaceDetailController {
     private final GetSpaceDetailService getSpaceDetailService;
 
     @GetMapping("/{spaceId}")
-    public ResponseEntity<?> getDetail(
+    public ResponseDto<SpaceDetailResponseDto> getDetail(
             @AuthenticationPrincipal User user,
-//            @RequestAttribute("userId") Long userId,
             @PathVariable Long spaceId
     ) {
         if (user == null) throw new BusinessException(AuthErrorCode.UNAUTHORIZED);
 
-        var data = getSpaceDetailService.execute(user.userId(), spaceId);
+        SpaceDetailResponseDto data = getSpaceDetailService.execute(user.userId(), spaceId);
 
-        return ResponseEntity.ok(Map.of(
-                "code", 200,
-                "message", "공간 상세 조회 성공",
-                "data", data
-        ));
+        return new ResponseDto<>(
+                HttpStatus.OK.value(),
+                "매물 상세 정보 조회 성공",
+                data
+        );
     }
 }
