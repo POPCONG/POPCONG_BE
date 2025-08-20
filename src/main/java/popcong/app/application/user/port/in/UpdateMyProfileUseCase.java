f6434756831bdbc07c59f1e280986a852e769c2e
seoiiwon <wldnjstj99@gmail.com>
2025-08-20 15:44:16 +0900

    🔨 Fix: "userRole 제거"
    UserRole 제거
    
    related to: #69

--- src/main/java/popcong/app/application/user/port/in/UpdateMyProfileUseCase.java
+++ src/main/java/popcong/app/application/user/port/in/UpdateMyProfileUseCase.java
@@ -1,13 +1,11 @@
 package popcong.app.application.user.port.in;
 
-import popcong.app.domain.user.model.UserRole;
 
 public interface UpdateMyProfileUseCase {
     void update(Long userId,
                 String name,
                 String introduction,
-                String profileImageUrl,
-                UserRole role
+                String profileImageUrl
     );
 
 }
--- src/main/java/popcong/app/application/user/port/out/UpdateUserPort.java
+++ src/main/java/popcong/app/application/user/port/out/UpdateUserPort.java
@@ -4,5 +4,5 @@ import popcong.app.domain.user.model.UserRole;
 
 public interface UpdateUserPort {
 
-    void updateProfile(Long userId, String name, String introduction, String profileImageUrl, UserRole role);
+    void updateProfile(Long userId, String name, String introduction, String profileImageUrl);
 }
